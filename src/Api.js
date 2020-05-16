2c48c9149bfca8de690eee3aab1b63593f98f30f
Abhishek Sharma <abhisheksharma.bvm@gmail.com>
2020-05-16 18:06:00 +0530

    First Commit

---
+++ src/Api.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Api extends Component{
+    state = {
+        data: [],
+    }
+    componentDidMount() {
+        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+        fetch(url)
+            .then(result => result.json())
+            .then(result=> {
+                this.setState({
+                    data: result,
+                })
+            })
+    }
+}
