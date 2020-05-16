2e1d69f86648948ba0fb387923d52f170bc7c469
Abhishek Sharma <abhisheksharma.bvm@gmail.com>
2020-05-16 20:01:22 +0530

    Updates

---
+++ precache-manifest.3d97ea32794d1a8549ce05c58427c0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528b6831be5752df6bd81973a541911f",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "824513bcfd31a02ed387",
+    "url": "/myapp/static/css/main.0fb20b90.chunk.css"
+  },
+  {
+    "revision": "c750ad45df555a5a79a8",
+    "url": "/myapp/static/js/2.e6844fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.e6844fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824513bcfd31a02ed387",
+    "url": "/myapp/static/js/main.9cad88ae.chunk.js"
+  },
+  {
+    "revision": "b54931f644491c988337",
+    "url": "/myapp/static/js/runtime-main.8fe3ce47.js"
+  }
+]);
